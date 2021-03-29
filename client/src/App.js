d623997632976b844bc83941ef1c8ae7727d7d3c
TravisBuilds <dev@lumierevr.com>
2021-03-30 00:41:17 +0800

    switched to ether.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-
+import Web3 from 'web3';
+import Web3Provider, {Connectors } from 'web3-react'; 
+import { ethers } from 'ethers';
 import GlobalStyle, {ThemeProvider} from './theme'; 
 import{Router, Route, BrowserRouter, Redirect, Switch} from 'react-router-dom'; 
 import {UserProvider} from './contexts/UserProvider'; 
-
+import Web3ReactManager from './components/Web3ReactManager';
 import {Home} from "./pages/Home";
 import {Profile} from "./pages/Profile";
 import {Trade} from "./pages/Trade";
 import {Discover} from "./pages/Discover";
 
 
-
-
 function App() {
+
+  const { InjectedConnector, NetworkOnlyConnector } = Connectors 
+  const Metamask = new InjectedConnector ({ supportedNetworks: [1,4]})
   
+  const Infura = new NetworkOnlyConnector({
+    providerURL: 'https://mainnet.infura.io/v3/...'
+  })
+
+  const connectors = {Metamask, Infura} 
+
   return (
     <ThemeProvider>
       <GlobalStyle>
