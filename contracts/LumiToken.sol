11a8b9d5ccc3a40a9b5425db20752aaadc4cf315
Jack Qiao <jack@lumierevr.com>
2021-03-30 22:51:45 -0400

    commented out unrelated function
    - Since our product tokens are not continuous, all the functions related to a continuous token implementation are commented out.
    - Places which needs to be changed are commented.

--- contracts/LumiToken.sol
+++ contracts/LumiToken.sol
@@ -26,50 +26,65 @@ contract LumiToken is ERC20, Ownable, BancorBondingCurve {
 
   function mint() public payable {
     require(msg.value > 0, "Must send ether to buy tokens.");
-    _continuousMint(msg.value);
+    _discreteMint(msg.value);
   }
 
- 	function burn(uint256 _amount) public {
-    uint256 returnAmount = _continuousBurn(_amount);
+ 	function sale(uint256 _amount) public {
+    uint256 returnAmount = _discreteSale(_amount);
     msg.sender.transfer(returnAmount);
   }
 
+  // need a burn function here.
+
   // function () public payable { mint(); }
 
-  function calculateContinuousMintReturn(uint256 _amount)
+  function calculateBuyReturn(uint256 _amount)
     public view returns (uint256 mintAmount)
   {
     return calculatePurchaseReturn(totalSupply(), reserveBalance, uint32(reserveRatio), _amount);
   }
 
-  function calculateContinuousBurnReturn(uint256 _amount)
+  function calculateSellReturn(uint256 _amount)
     public view returns (uint256 burnAmount)
   {
     return calculateSaleReturn(totalSupply(), reserveBalance, uint32(reserveRatio), _amount);
   }
 
-  function _continuousMint(uint256 _deposit)
+  function _discreteMint(uint256 _deposit)
     internal returns (uint256)
   {
     require(_deposit > 0, "Deposit must be non-zero.");
 
-    uint256 amount = calculateContinuousMintReturn(_deposit);
+    uint256 amount = calculateBuyReturn(_deposit);
+    // have to make it discrete here.
+
     _mint(msg.sender, amount);
     reserveBalance = reserveBalance.add(_deposit);
     emit Minted(msg.sender, amount, _deposit);
     return amount;
   }
 
-  function _continuousBurn(uint256 _amount)
+  // have to add a function about redeeming token
+  function _discreteSale(uint256 _amount)
     internal returns (uint256)
+  {
+  	// uint256 reimburseAmount = calculateSellReturn(_amount);
+ 		// reserveBalance = reserveBalance.sub(reimburseAmount);
+
+  }
+
+  // have to change return types, since burning a product token have a different set of logics.
+  function _discreteBurn(uint256 _amount)
+    internal
   {
     require(_amount > 0, "Amount must be non-zero.");
     require(balanceOf(msg.sender) >= _amount, "Insufficient tokens to burn.");
 
-    uint256 reimburseAmount = calculateContinuousBurnReturn(_amount);
-    reserveBalance = reserveBalance.sub(reimburseAmount);
+    // uint256 reimburseAmount = calculateSellReturn(_amount);
+    // reserveBalance = reserveBalance.sub(reimburseAmount);
+    // have to change burn function here
     _burn(msg.sender, _amount);
-    emit Burned(msg.sender, _amount, reimburseAmount);
-    return reimburseAmount;
+    // emit Burned(msg.sender, _amount, reimburseAmount);
+    // return reimburseAmount;
   }
 }
