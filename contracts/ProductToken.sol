5d5e8e6ea54d0cc7ad3ca4031522f01df70620fd
Jack Qiao <jack@lumierevr.com>
2021-04-02 15:52:24 -0400

    Adjusted token logic based on new math functions
    - Adjusted token logics in ProductToken.sol according to the adjustment in the math functions from BancorBondingCurve.sol
    - Rename some of the functions to be coherent with business/product logic.

--- contracts/ProductToken.sol
+++ contracts/ProductToken.sol
@@ -7,9 +7,9 @@ import "./BancorBondingCurve.sol";
 contract ProductToken is ERC20, Ownable, BancorBondingCurve {
 	using SafeMath for uint256;
 
-	event Minted(address sender, uint32 amount, uint deposit);		// event to fire when a new token is minted
-  event Traded(address sender, uint32 amount, uint refund);			// event to fire when a token has been sold back
-  event Burned(address sender, uint32 amount);									// event to fire when a token is redeemed in the real world
+	event Buy(address sender, uint32 amount, uint deposit);		// event to fire when a new token is minted
+  event Sell(address sender, uint32 amount, uint refund);			// event to fire when a token has been sold back
+  event Tradein(address sender, uint32 amount);									// event to fire when a token is redeemed in the real world
 
   uint256 public reserveBalance = 330000000000000000;		// amount in ether, about 1/3 of a ether. This is initialized for testing, according to
                                                         // a pricing function of y = x ^ 2, at a token supply (x) of 1
@@ -17,9 +17,7 @@ contract ProductToken is ERC20, Ownable, BancorBondingCurve {
   uint256 public reserveRatio;
 
   uint32 public maxTokenCount;
-  uint32 public burnedCount = 0;
-
-  // mapping (address => uint256) ownerTokenCount;
+  uint32 public tradeinCount = 0;
 
 	/**
    * @dev Constructor
@@ -30,9 +28,10 @@ contract ProductToken is ERC20, Ownable, BancorBondingCurve {
   constructor(uint32 _exponent, uint32 _maxTokenCount) ERC20("ProductToken", "") public {		
   	require(_maxTokenCount > 0, "Invalid max token count.");
   	exponent = _exponent;
-    reserveRatio = 330000; // uint256(1000000).div(uint256(_exponent).add(1));		// initialize the reserve ratio for this token in ppm. This is hardcoded right now because we are testing with 33%
+    reserveRatio = uint256(1000000).div(uint256(_exponent).add(1));		// initialize the reserve ratio for this token in ppm. 
+                                                                      // This is hardcoded right now because we are testing with 33%
     maxTokenCount = _maxTokenCount;
-    _mint(msg.sender, 1);
+    _mint(msg.sender, 1);  
   }
 
   /**
@@ -41,7 +40,11 @@ contract ProductToken is ERC20, Ownable, BancorBondingCurve {
   */
   function buy() public payable {
     require(msg.value > 0, "Must send ether to buy tokens.");
-    _discreteMint(msg.value);
+    uint256 amount;
+    uint256 change;
+    (amount, change) = _buyForAmount(msg.value);
+    // return change back to the sender.
+    msg.sender.transfer(change);
   }
 
 	/**
@@ -49,86 +52,115 @@ contract ProductToken is ERC20, Ownable, BancorBondingCurve {
    *
   */
  	function sell(uint32 _amount) public {
-    uint256 returnAmount = _discreteSale(_amount);
+    uint256 returnAmount = _sellForAmount(_amount);
     msg.sender.transfer(returnAmount);
   }
 
 	/**
    * @dev When user wants to trade in their token for retail product
-   * the logistics for transfering product should be handled by the front end
+   * the logistics for transfering product should be handled elsewhere
    *
   */
   function tradein(uint32 _amount) public {
-  	_discreteBurn(_amount);
+  	_tradeinForAmount(_amount);
   }
 
   // function () public payable { mint(); }
 
   // View Functions for outside.
-
-  // Remember this is a view function. Removed view modifier for testing only
   function getCurrentPrice() 
   	public view returns	(uint256 price)
   {
-  	return calculateUnitPrice(totalSupply() + burnedCount, reserveBalance, uint32(reserveRatio));
+  	return calculateUnitPrice(totalSupply() + tradeinCount, reserveBalance, uint32(reserveRatio));
   }
 
   function getPriceForN(uint32 _amount) 
   	public view returns	(uint256 price)
   {
-  	return calculatePriceForNTokens(totalSupply() + burnedCount, reserveBalance, exponent, _amount);
+  	return calculatePriceForNTokens(totalSupply() + tradeinCount, reserveBalance, exponent, _amount);
   }
 
   function calculateBuyReturn(uint256 _amount)
     public view returns (uint256 mintAmount)
   {
-    return calculatePurchaseReturn(totalSupply() + burnedCount, reserveBalance, uint32(reserveRatio), _amount);
+    return calculatePurchaseReturn(totalSupply() + tradeinCount, reserveBalance, uint32(reserveRatio), _amount);
   }
 
   function calculateSellReturn(uint256 _amount)
     public view returns (uint256 burnAmount)
   {
-    return calculateSaleReturn(totalSupply() + burnedCount, reserveBalance, uint32(reserveRatio), _amount);
+    return calculateSaleReturn(totalSupply() + tradeinCount, reserveBalance, uint32(reserveRatio), _amount);
   }
 
   // specific implementations of transaction logics.
-  function _discreteMint(uint256 _deposit)
-    internal returns (uint256)
+   /**
+   * @dev calculates the return for a given conversion (in product token) 
+   * This function will try to compute the amount of tokens one can buy first, 
+   * then it will initiate a transfer and for any extras, return as change.
+   * if any tokens purchases are confirmed, it will update ownerTokenCount mappin. 
+   *
+   * @param _deposit              reserve total deposited
+   *
+   * @return token amount bought
+  */
+  function _buyForAmount(uint256 _deposit)
+    internal returns (uint256, uint256)
   {
-  	require(totalSupply() + burnedCount < maxTokenCount, "Sorry, this token is sold out.");
+  	require(totalSupply() + tradeinCount < maxTokenCount, "Sorry, this token is sold out.");
     require(_deposit > 0, "Deposit must be non-zero.");
 
     uint256 amount = calculateBuyReturn(_deposit);	    // have to make it discrete here. Replace amount with uint32
-
-    _mint(msg.sender, amount);		// have to specifically mint whole numbers
-    reserveBalance = reserveBalance.add(_deposit);
-    // emit Minted(msg.sender, amount, _deposit);		// need to change amount to precise token counts.
-    return amount;
+    // If the amount in _deposit is more than enough to buy out the rest of the token in the pool
+    if (amount > maxTokenCount.sub(totalSupply()).sub(tradeinCount)) {   // this logic can be refactored.
+      amount = maxTokenCount.sub(totalSupply()).sub(tradeinCount);
+    }
+    uint256 actualDeposit = calculatePriceForNTokens(amount);
+    _mint(msg.sender, amount);
+    reserveBalance = reserveBalance.add(actualDeposit);
+    emit Buy(msg.sender, amount, actualDeposit);		// probably needs to be redesigned for ease of read and understanding.
+    return (amount, _deposit.sub(actualDeposit));    // return amount of token bought and change
   }
 
-  // have to add a function about redeeming token
-  function _discreteSale(uint32 _amount)
+   /**
+   * @dev calculates the return for a given conversion (in the reserve token) 
+   * This function will try to compute the amount of liquidity one gets by selling one token, 
+   * then it will initiate a transfer.
+   * said token then will be burned to reduce total supply, but won't be added to the tradeinCount variable 
+   *
+   * @param _amount              product token wishes to be sold
+   *
+   * @return token amount bought
+  */
+  function _sellForAmount(uint32 _amount)
     internal returns (uint256)
   {
   	require(_amount > 0, "Amount must be non-zero.");
     require(balanceOf(msg.sender) >= _amount, "Insufficient tokens to sell.");
-
-  	// uint256 reimburseAmount = calculateSellReturn(_amount);
- 		// reserveBalance = reserveBalance.sub(reimburseAmount);
-
+    // calculate amount of liquidity to reimburse
+  	uint256 reimburseAmount = calculateSellReturn(_amount);
+ 		reserveBalance = reserveBalance.sub(reimburseAmount);
+    _burn(msg.sender, _amount);
+    emit Sell(msg.sender, _amount, reimburseAmount);
+    return reimburseAmount;
   }
 
-  // have to change return types, since burning a product token have a different set of logics.
-  function _discreteBurn(uint32 _amount)
+
+   /**
+   * @dev initiate token logics after a token is traded in. 
+   * This function only handles logics corresponding to 
+   *
+   * @param _amount              product token wishes to be traded-in
+   *
+   * @return token amount bought
+  */
+  function _tradeinForAmount(uint32 _amount)
     internal 
   {
     require(_amount > 0, "Amount must be non-zero.");
     require(balanceOf(msg.sender) >= _amount, "Insufficient tokens to burn.");
 
-    // have to change burn function here
     _burn(msg.sender, _amount);
-    // emit Burned(msg.sender, _amount, reimburseAmount);
-    burnedCount = burnedCount + _amount;			// Future: use safe math here.
-    // return reimburseAmount;
+    tradeinCount = tradeinCount + _amount;			// Future: use safe math here.
+    emit Burned(msg.sender, _amount);
   }
 }
