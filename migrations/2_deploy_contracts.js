1ee6bff4c0e8ac44b91c77306c742ea9d8a59b4a
Jack Qiao <jack@lumierevr.com>
2021-04-27 19:36:59 -0400

    Deployment and test files modified for interactive testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,8 @@
 const Factory = artifacts.require("TokenFactory");
 
 module.exports = async function (deployer, network) {
-	// let tokenAddress;
-	if (network === "mainnet") {
-		// tokenAddress = "";
-	} else {
-		// await deployer.deploy(Token, 330000, 500, 10, web3.utils.toWei('0.33', 'ether'));			// initialize reserve ratio for the token in ppm, stand in for testing.
-		// const token = await Token.deployed();
-		// tokenAddress = token.address;
-		await deployer.deploy(Factory);
-		const factory = await Factory.deployed();
-	}
+	// await deployer.deploy(Token, 330000, 500, 3, web3.utils.toWei('9', 'ether'));			// initialize reserve ratio for the token in ppm, stand in for testing.
+	// const token = await Token.deployed();
+	await deployer.deploy(Factory);
+	const factory = await Factory.deployed();
 };
