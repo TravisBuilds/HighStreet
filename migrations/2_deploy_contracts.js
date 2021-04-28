26a5655c5237d6f2f6ec72a9338e726af902025d
Jack Qiao <jack@lumierevr.com>
2021-04-28 17:26:39 -0400

    Updated project migration files and config for deployment to testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 // const Token = artifacts.require("ProductToken");
 const Factory = artifacts.require("TokenFactory");
 
-module.exports = async function (deployer, network) {
+module.exports = async function (deployer, network, accounts ) {
 	// await deployer.deploy(Token, 330000, 500, 3, web3.utils.toWei('9', 'ether'));			// initialize reserve ratio for the token in ppm, stand in for testing.
 	// const token = await Token.deployed();
-	await deployer.deploy(Factory);
+	await deployer.deploy(Factory, {from: accounts[0]});
 	const factory = await Factory.deployed();
-	let token = await factory.createToken("Kalon Tea", 330000, 500, 3, web3.utils.toWei('9', 'ether'));
+	let token = await factory.createToken("Kalon Tea", 330000, 500, 3, web3.utils.toWei('9', 'ether'), {from: accounts[0]});
 	// let address = await factory.retrieveToken("Kalon Tea");		// this is how you retrieve token.	
 };
