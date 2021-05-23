741c1656855d6a1b7d620f7ef2f720eb8e9d4e8c
hekegang <hkg10918@ly.com>
2021-05-23 23:05:01 +0800

    BeanUpgrableV2.test.js

--- client/src/contracts/TokenProxyFactory.sol
+++ client/src/contracts/TokenProxyFactory.sol
@@ -2,18 +2,20 @@ pragma solidity ^0.8.2;
 
 import {ClonesUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
 import "./ProductToken.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+
 
 contract TokenProxyFactory { 
 
 	address public owner;
 	address daiAddress;
-    address public impContract;
+    address public beacon;
 	mapping(string => address) registry;
 
-	constructor(address _daiAddress,address _impContract) public {
+	constructor(address _daiAddress,address _beacon) public {
 		owner = msg.sender;
 		daiAddress = _daiAddress;
-        impContract=_impContract;
+        beacon=_beacon;
 	}
 
 	modifier onlyOwner {
@@ -30,7 +32,8 @@ contract TokenProxyFactory {
 
 	function createTokenProxy(string memory _productName, bytes memory _data) public onlyOwner returns (address) {
 		require(registry[_productName]==address(0), "The product token already exist");
-		address newProxyToken = ClonesUpgradeable.cloneDeterministic(impContract, _data);
+		// address newProxyToken = ClonesUpgradeable.cloneDeterministic(impContract, _data);
+		address newProxyToken = address(new BeaconProxy(beacon,_data));
 		registry[_productName] = newProxyToken;
 		return newProxyToken;
 	}
