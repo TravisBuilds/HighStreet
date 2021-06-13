97259cab69e32c1c782cf70cb9a6ee1aa42e3d34
JackShiu <poming.shiu@gmail.com>
2021-06-13 11:37:17 +0800

    Support buy token by dai on Arbitrum

--- client/src/contexts/ProductProvider.jsx
+++ client/src/contexts/ProductProvider.jsx
@@ -3,132 +3,129 @@ import React, { createContext, useReducer } from 'react';
 import { ethers } from 'ethers';
 import { Bridge } from 'arb-ts';
 import ProductReducer from './ProductReducer';
-import kalonCard from '../assets/product1.png';
-import loreal from '../assets/product2.png';
 import mystery from '../assets/product3.png';
-import lvmh from '../assets/product4.png';
-import kalonfeature from '../assets/kalon.png';
-import lvmhfeature from '../assets/lvmh.png';
-import lorealfeature from '../assets/loreal.png';
 import randomfeature from '../assets/randomfeature.png';
 // abstract bridge interface for solidity
-import Token from '../build/contracts/ProductToken.json';
+import FactoryProxy from '../build/contracts/ERC1967Proxy.json';
 import Factory from '../build/contracts/TokenFactory.json';
-// import Dai from '../build/contracts/DaiMock.json';
+import TokenV1 from '../build/contracts/ProductTokenV1.json';
+import ERC20 from '../build/contracts/IERC20.json';
 
-// class level variables that store much needed handles
+// +++ template fix UI error
+const kalonCard = mystery;
+const loreal = mystery;
+const lvmh = mystery;
+const kalonfeature = randomfeature;
+const lvmhfeature = randomfeature;
+const lorealfeature = randomfeature;
+// --- template fix UI error
+
+const DBUG = true;
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner(); // this is the account that user uses to make payment.
-let factoryContract; // this handle is used to call view functions from the smart contract.
-// let factoryContractWSigner;  // this handle is used to call state-changing functions from the smart contract.
-// let daiAddress; // this is the address for Dai tokens, it is pre-defined based on which network user is connected to.
-// let daiContract;
-// let daiContractWSigner;
-// function that initialize the solidity smart contract handles
-// async fu nction initializeNetwork() {
-provider.getNetwork().then((result) => {
-  console.log(`Network Retrieved: ${result}`);
-  const networkId = result.chainId;
-  console.log(`Network ID: ${networkId}`);
-  // switch (networkId) {
-  //   case 1: // This is for Main Net
-  //     console.log('Main net selected');
-  //     daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-  //     break;
-  //   case 42: // This is for Kovan Test Net
-  //     console.log('Kovan net selected');
-  //     daiAddress = '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa';
-  //     break;
-  //   case 1337:
-  //     console.log('Ganache local net selected');
-  //     daiAddress = Dai.networks[networkId].address;
-  //     break;
-  //   default:
-  //     console.log('Dai is not supported on other platforms, an error should be thrown');
-  //     daiAddress = '';
-  // }
+const signer = provider.getSigner();
+let isValidNetowrk;
 
-  const networkData = Factory.networks[networkId];
-  if (networkData) {
-    // console.log('Ready to connect to contract.');
-    // console.log(`Dai Address is set to ${daiAddress}`);
-    factoryContract = new ethers.Contract(networkData.address, Factory.abi, provider);
-    // daiContract = new ethers.Contract(daiAddress, Dai.abi, provider);
-    // daiContractWSigner = daiContract.connect(signer);
-    // factoryContractWSigner = factoryContract.connect(signer);
-  } else {
-    console.log("Contract wasn't deployed properly.");
-  }
-}).catch((e) => {
-  console.log(e);
-});
-// }
+// factoryObj
+let factoryAddress;
+let factoryContract;
 
+// tokenObj
 let tokenAddress;
-let token; // this is the token handle for a particular token user is interested.
-// for the following functions, I assume that token has been set accordingly.
-let tokenWSigner; // again, this is the token handle that is required to make any state changing method calls
-// const maker = Maker.create('test');
+let tokenContract;
+let tokenSigner;
 
-// await maker.authenticate();
-// const tokenService = maker.service('token');
-// const dai = tokenService.getToken('DAI');
+// daiObj
+let daiAddress;
+let daiContract;
+let daiSigner;
 
-// This function retrieve the address of a token by its name.
-// then it creates a token handle with
+async function handleChainChanged(_chainId) {
+  switch (_chainId) {
+    case 4:
+      console.log('(L1)Rinkeby Testnet');
+      daiAddress = '0xc7ad46e0b8a400bb3c915120d284aafba8fc4735';
+      await updateContract(_chainId);
+      break;
+    case 421611:
+      console.log('(L2)Arbitrum Testnet');
+      daiAddress = '0x552444108a2aF6375205f320F196b5D1FEDFaA51';
+      await updateContract(_chainId);
+      break;
+    default:
+      isValidNetowrk = false;
+      console.log('not support !!');
+  }
+}
+
+/*
+* Listening MetaMask change chain
+*/
+window.ethereum.on('chainChanged', handleChainChanged);
+/*
+* Get network Id at beginning
+*/
+provider.getNetwork().then(async (result) => handleChainChanged(result.chainId));
+
+async function updateContract(_chainId) {
+  isValidNetowrk = true;
+  factoryAddress = FactoryProxy.networks[_chainId].address;
+  factoryContract = new ethers.Contract(factoryAddress, Factory.abi, provider);
+  daiContract = new ethers.Contract(daiAddress, ERC20.abi, provider);
+  daiSigner = await daiContract.connect(signer);
+}
+
 async function retrieveTokenByName(name) {
-  await factoryContract.retrieveToken(name).then((result) => {
+  if (!isValidNetowrk) return;
+
+  const prodName = 'HighGO';
+  await factoryContract.retrieveToken(prodName).then(async (result) => {
+    if (DBUG) console.log(`product: ${prodName}, address: ${result}`);
     tokenAddress = result;
-    token = new ethers.Contract(tokenAddress, Token.abi, provider);
-    tokenWSigner = token.connect(signer);
+    tokenContract = new ethers.Contract(tokenAddress, TokenV1.abi, provider);
+    tokenSigner = tokenContract.connect(signer);
   }).catch((e) => {
     console.log(e);
   });
 }
 
 function getAvailability() {
-  return token.getAvailability();
+  return tokenContract.getAvailability();
 }
 
 function getPrice() {
-  return token.getCurrentPrice();
+  return tokenContract.getCurrentPrice();
 }
 
 function getPriceForN(tokens) { // token must be a number that's smaller than 2^32 - 1
-  return token.getPriceForN(tokens);
+  return tokenContract.getPriceForN(tokens);
 }
 
-async function buy(cashUpperBound) { // purchase tokens based on a cash upper bound
-  // need to implment DAI handle
-  // Dai handle then needs to approve the above amount to be withdrawn
-  // here we assume that cashUpperBound has already been parsed to 18 decimals
-  // await daiContractWSigner.approve(tokenAddress, cashUpperBound).then(async () => {
-  //   tokenWSigner.buy(cashUpperBound);
-  // }).catch((e) => {
-  //   console.log(e);
-  // });
-  const overrides = {
-    // To convert Ether to Wei:
-    value: ethers.utils.parseEther('1.0') // ether in this case MUST be a string
+const formatDaiUnits = async (val) => ethers.utils.formatUnits(val.toString(), 18);
 
-    // Or you can use Wei directly if you have that:
-    // value: someBigNumber
-    // value: 1234   // Note that using JavaScript numbers requires they are less than Number.MAX_SAFE_INTEGER
-    // value: "1234567890"
-    // value: "0x1234"
+async function buy(cashUpperBound) {
+  if (!isValidNetowrk) return;
 
-    // Or, promises are also supported:
-    // value: provider.getBalance(addr)
-  };
-  await tokenWSigner.buy('1', overrides);
+  const curTokenPrice = await tokenContract.getCurrentPrice();
+  console.log(`current product price ${await formatDaiUnits(curTokenPrice)}`);
+
+  const approvlPrice = curTokenPrice.mul('15').div('10');
+  console.log(`send dai amount(price * 1.5): ${await formatDaiUnits(approvlPrice)}`);
+
+  await daiSigner.approve(tokenAddress, approvlPrice).then(async () => {
+    console.log('GET APPROVE');
+    await tokenSigner.buyWithDai(approvlPrice, '1');
+  }).catch((e) => {
+    console.log(e);
+  });
 }
 
 async function sell(tokenAmount) { // token must be a number that's smaller than 2^32 - 1
-  await tokenWSigner.sell('1');
+  if (!isValidNetowrk) return;
+  await tokenSigner.sell('1');
 }
 
 async function tradeIn(tokenAmount) { // token must be a number that's smaller than 2^32 - 1
-  await tokenWSigner.tradein(tokenAmount);
+  await tokenSigner.tradein(tokenAmount);
 }
 
 // import ProductToken.sol
