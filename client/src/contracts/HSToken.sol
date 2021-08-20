dc2f341557f2687bb73e3dd86a842a7b8d765d46
Jack Qiao <jack@lumierevr.com>
2021-08-20 10:19:03 -0400

    Update HSToken.sol
    - deleted logic from HSToken that's outdated.

--- client/src/contracts/HSToken.sol
+++ client/src/contracts/HSToken.sol
@@ -14,95 +14,12 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract HSToken is ERC20, Ownable {
     uint256 private constant maxSupply = 100000000 * 1e18;     // the total supply
 
-    using EnumerableSet for EnumerableSet.AddressSet;
-    using SafeMath for uint256;
-    EnumerableSet.AddressSet private _minters;
-
-    event AddMinter(address newMinter);
-    event DelMinter(address deleltedMinter);
-
     /**
     * @dev constructor function.
     *
     */
     constructor() public ERC20("Street Token", "TKN"){
-        // _mint(msg.sender, preMineSupply);
-        addMinter(msg.sender);
-    }
-
-    /**
-    * @dev mint function. mint tokens to a designated address.
-    *
-    * @param _to                    the designated address.
-    * @param _amount                number of tokens to be minted to an address.
-    * @return bool                  status of mint
-    */
-    function mint(address _to, uint256 _amount) external onlyMinter returns (bool) {
-        if (_amount.add(totalSupply()) > maxSupply) {
-            return false;
-        }
-        _mint(_to, _amount);
-        return true;
-    }
-
-    /**
-    * @dev grant an address minting power.
-    *
-    * @param _addMinter             the address to be added as a minter.
-    * @return bool                  status of add
-    */
-    function addMinter(address _addMinter) public onlyOwner returns (bool) {
-        require(_addMinter != address(0), "HSToken: _addMinter is the zero address");
-        emit AddMinter(_addMinter);
-        return EnumerableSet.add(_minters, _addMinter);
-    }
-
-    /**
-    * @dev remove an address of its minting power.
-    *
-    * @param _delMinter             the address to be deleted from the minter list.
-    * @return bool                  status of remove
-    */
-    function delMinter(address _delMinter) external onlyOwner returns (bool) {
-        require(_delMinter != address(0), "HSToken: _delMinter is the zero address");
-        emit DelMinter(_delMinter);
-        return EnumerableSet.remove(_minters, _delMinter);
-    }
-
-    /**
-    * @dev return the length of the list of current minters.
-    *
-    * @return uint256               number of minters.
-    */
-    function getMinterLength() public view returns (uint256) {
-        return EnumerableSet.length(_minters);
-    }
-
-    /**
-    * @dev check whether an address has minter access.
-    *
-    * @param _account               the address in question.
-    * @return bool                  access confirmation.
-    */
-    function isMinter(address _account) public view returns (bool) {
-        return EnumerableSet.contains(_minters, _account);
-    }
-
-    /**
-    * @dev get minter address of the minter at particular index. This will be used with our iterator function.
-    *
-    * @param _index                 the index of minter.
-    * @return address               minter address.
-    */
-    function getMinter(uint256 _index) external view onlyOwner returns (address){
-        require(_index <= getMinterLength() - 1, "HSToken: _index out of bounds");
-        return EnumerableSet.at(_minters, _index);
-    }
-
-    // modifier for mint function
-    modifier onlyMinter() {
-        require(isMinter(msg.sender), "HSToken: It is not the minter");
-        _;
+        
     }
 
 } 
